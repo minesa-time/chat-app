a978ff595974b4e30a53d7fb59bbe4cd1dc2f4c6
Neodevils <neoaichan@gmail.com>
2023-01-16 00:15:52 +0300

    Not done yet.

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import LoginScreen from "./screens/LoginScreen";
+import ChatScreen from "./screens/ChatScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppNavigator = createStackNavigator(
+	{
+		Login: LoginScreen,
+		Chat: ChatScreen,
+	},
+	{
+		headerMode: "none",
+	}
+);
+
+AppRegistry.registerComponent('mns-chat-app', () => createAppContainer(AppNavigator));
+
+export default createAppContainer(AppNavigator);
